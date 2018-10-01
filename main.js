6354a6499aa2da43c73c419004cc160078d3d508
Ly Gia <hopehappydayno1@gmail.com>
2018-10-01 19:44:13 +0700

    mofi main

--- main.js
+++ main.js
@@ -2,63 +2,60 @@ const fs = require("fs");
 const login = require("facebook-chat-api");
 
 login({ email: "0978085495", password: "d1fc0nku" }, (err, api) => {
-	if (err) return console.error(err);
+    if (err) return console.error(err);
 
-	fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
 
-	api.setOptions({ forceLogin: true, selfListen: false, logLevel: "silent", listenEvents: true });
-	
+    api.setOptions({ forceLogin: true, selfListen: false, logLevel: "silent", listenEvents: true });
 
-	var ID=100006165366157;  // id trieu xuan khang
-	var ID2=100004359125508; // id ly gia
-	var IDG=1590386811002851; // id group chat
-	var url="https://goo.gl/3kY3An"; // sheets log
-	var pattern=/sp[0-9]*/ig; // chuoi chinh quy
-	var d=new Date();
-	var num=1;
-	var nums=1;
-	api.listen(function callback(err, message) {
-		if (message.body==="getid"||message.body==="Getid"||message.body==="GetID"||message.body==="/getid") {
-			
-			console.log("1. FB.com/" + message.threadID + ' - Message: ' + message.body+' - time : '+d.toUTCString());
 
-			api.sendMessage(nums,message.threadID);
-			api.sendMessage("ID : "+message.threadID,message.threadID);
-			api.sendMessage("time : "+d.toUTCString(),message.threadID);
-			nums++;
-		}
-		//Ví dụ nếu mình set message.body = /Jarvis thì bot sẽ print api.sendMessage = Send bobs n vegena... 
-		else if ((message.body === "ok" || message.body === "Ok")&&message.senderID==ID) {
+    var ID = 100006165366157; // id trieu xuan khang
+    var ID2 = 100004359125508; // id ly gia
+    var IDG = 1590386811002851; // id group chat
+    var url = "https://goo.gl/3kY3An"; // sheets log
+    var pattern = /sp[0-9]*/ig; // chuoi chinh quy
+    var d = new Date();
 
-			console.log("2. FB.com/" + message.threadID + ' - Message: ' + message.body+' - time : '+d.toUTCString());
+    api.listen(function callback(err, message) {
+        if (message.body === "getid" || message.body === "Getid" || message.body === "GetID" || message.body === "/getid") {
 
-			api.sendMessage(num+" : _____ok_____", ID);
+            console.log("1. FB.com/" + message.threadID + ' - Message: ' + message.body + ' - Time : ' + d.toUTCString());
 
-			api.sendMessage("ok chi : "+num,IDG);
-			
-			api.sendMessage(num+" : xac nhan dang sp",ID2)
-			api.sendMessage(" time : "+d.toUTCString(),ID2);
-			
-			num++;
-			return;
-		}
-		//message.body = tin nhắn bạn send cho bot
-		else if (pattern.exec(message.body)) {
-			// ghi log nguoi gui
-			console.log("3. FB.com/" + message.threadID + ' - Message: ' + message.body+' - time : '+d.toUTCString());
-			// gui phan hoi cho ktv
-			api.sendMessage(num+" : _____co sp moi nhe _____",ID);
-			api.sendMessage("Check log có khách thì sp nhẽ !!" , ID);
-			api.sendMessage("link :"+url, ID);
-			api.sendMessage("time : "+d.toUTCString(),ID);
+            api.sendMessage(nums, message.threadID);
+            api.sendMessage("ID : " + message.threadID, message.threadID);
+            api.sendMessage("Time : " + d.toUTCString(), message.threadID);
+            nums++;
+        }
+        //Ví dụ nếu mình set message.body = /Jarvis thì bot sẽ print api.sendMessage = Send bobs n vegena... 
+        else if ((message.body === "ok" || message.body === "Ok") && message.senderID == ID) {
 
-			// xac nhan da gui
-			api.sendMessage(num+" : xac nhan da gui thong tin sp",ID2)
-			api.sendMessage("time : "+d.toUTCString(),ID2);
-			
-			return;
-		}
-		
-	});
+            console.log("2. FB.com/" + message.threadID + ' - Message: ' + message.body + ' - time : ' + d.toUTCString());
 
+            api.sendMessage("_____ok_____", ID);
+
+            api.sendMessage("ok chi : " + num, IDG);
+
+            api.sendMessage("xac nhan dang sp", ID2)
+            api.sendMessage(" Time : " + d.toUTCString(), ID2);
+
+            num++;
+            return;
+        }
+        //message.body = tin nhắn bạn send cho bot
+        else if (pattern.exec(message.body)) {
+            // ghi log nguoi gui
+            console.log("3. FB.com/" + message.threadID + ' - Message: ' + message.body + ' - Time : ' + d.toUTCString());
+            // gui phan hoi cho ktv
+            api.sendMessage("_____co sp moi nhe _____", ID);
+            api.sendMessage("Check log có khách thì sp nhẽ !!", ID);
+            api.sendMessage("link :" + url, ID);
+            api.sendMessage("Time : " + d.toUTCString(), ID);
+
+            // xac nhan da gui
+            api.sendMessage("xac nhan da gui thong tin sp", ID2)
+            api.sendMessage("Time : " + d.toUTCString(), ID2);
+
+            return;
+        }
+    });
 });
