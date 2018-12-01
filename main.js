f98bf35f808fc4dc8ef7bcfb4a34a7cfa4135313
Ly Gia <HopeHappyDayNo1@gmail.com>
2018-12-01 10:55:46 +0700

    restet

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const login = require("facebook-chat-api");
 
-login({ email: "0978085495", password: "b0tchatfacebook.," }, (err, api) => {
+login({ email: "0978085495", password: "c0ngvi3cm4.," }, (err, api) => {
     if (err) return console.error(err);
 
     fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
